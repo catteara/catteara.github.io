b67171ef87478c1af47ccbbcfe95d8ed4af48e01
catteara <cattearachristine@gmail.com>
2021-01-14 19:16:54 -0700

    63rd Commit

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+"use strict";
+
+    var dayNames = [
+        "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+    ];
+    var months = [
+        "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    ];
+    var d = new Date();
+    var dayName = dayNames[d.getDay()];
+    var monthName = months[d.getMonth()];
+    var fullDate = d.getHours() + ":" + d.getMinutes() + " " + dayName + ", " + monthName + " " + d.getDate() + ", " + d.getFullYear();
+
+    document.getElementById("rightNow").innerHTML = "Last Updated: " + fullDate;
+
