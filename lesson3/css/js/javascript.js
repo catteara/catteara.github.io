c462cd55ef61289005e47baeeefef76095b44e20
catteara <cattearachristine@gmail.com>
2021-01-23 23:45:59 -0700

    73rd Commit

---
+++ lesson3/css/js/javascript.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//used w3schools to help
+
+function menu() {
+   
+    var x = document.getElementById("links");
+     if (x.style.display === "block") {
+        x.style.display = "none";
+     } else {
+         x.style.display = "block"
+     }
+
+}
