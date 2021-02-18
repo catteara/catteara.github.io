e0c7884ff454ff869ed044f56348034427d5c4cd
catteara <cattearachristine@gmail.com>
2021-02-18 15:23:09 -0700

    96th Commit

--- lesson7/js/windchill.js
+++ lesson7/js/windchill.js
@@ -3,8 +3,4 @@ var s = document.getElementById("windSpeed").innerHTML;
 
 var f = Math.round(35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16)));
 
-if(t>50||s<3) {
-    document.getElementById("windChill").innerHTML =  " 0";
-} else {
-    document.getElementById("windChill").innerHTML =  f;
-}
+document.getElementById("windChill").innerHTML =  f;
