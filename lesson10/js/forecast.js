53740a419e104c853f98b21730b024d24e4762eb
catteara <cattearachristine@gmail.com>
2021-03-19 21:47:15 -0700

    115 Commit

--- lesson10/js/forecast.js
+++ lesson10/js/forecast.js
@@ -1,13 +1,41 @@
-const URL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=9f6e867aadd6669d3ad428534541ab88';
 
+const URL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=9f6e867aadd6669d3ad428534541ab88'
 fetch(URL)
   .then((response) => response.json())
-  .then((jsonObject) => {
-    console.log(jsonObject);
-    const list = jsonObject['list'];
+  .then((jsObject) => {
 
-    //Week Days
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    
+    for (i=0; i < jsObject.list.length; i++) {
+        if (jsObject.list[i].dt_txt.includes("18:00:00") == true) {
+            
+            let div = document.createElement('div');
+            let p1 = document.createElement('p');
+            let p2 = document.createElement('p');
+            let image = document.createElement('img');
+            let d = new Date(jsObject.list[i].dt_txt);
+            let dayName = days[d.getDay()];
+           
+            p1.textContent = dayName;
+            image.setAttribute('src', 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png');
+            image.setAttribute('alt', "weather icon");
+            p2.textContent = jsObject.list[i].main.temp_max.toFixed(0) + "\xB0" + " F";
+            
+            div.appendChild(p1);
+            div.appendChild(image);
+            div.appendChild(p2);
+            
+            
+            document.querySelector('div.forecast').appendChild(div);
+            
+        }
+    }
 
+
+
+});
+
+        /* 
         var now = new Date(list[0].dt_txt);
         var today = now.getDate(); 
         var dayOfMonth = new Date().getDate();
@@ -73,3 +101,4 @@ fetch(URL)
         document.getElementById('low5').textContent = list[4].main.temp_min + '\xB0';
   });
  
+  */
